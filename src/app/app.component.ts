332cb3eeff312fcdc604ff3737b60f7baa4081b0
Aptir <cesar.arzapalo@unmsm.edu.pe>
2022-01-18 17:56:38 -0500

    almacenando cambios de aplicaicion web de geolocalizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Map, marker, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'geolocalizacion';
+  map:any;
+  cordsLat: number = 0;
+  cordsLon: number = 0;
+
+  ngOnInit(): void {
+    this.locate();
+  }
+
+  locate(): void {
+    if(!navigator.geolocation) {
+      console.log("No esta soportado la localizacion");
+    }
+ 
+    navigator.geolocation.getCurrentPosition( (position) => {
+      this.cordsLat = position.coords.latitude;
+      this.cordsLon = position.coords.longitude;
+      console.log(
+        `lat: ${this.cordsLat}, lon: ${this.cordsLon}`
+      );
+      this.map = new Map('map').setView([this.cordsLat, this.cordsLon], 17);
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(this.map);
+     
+      marker([this.cordsLat, this.cordsLon]).addTo(this.map);
+    });
+  }
+
 }
