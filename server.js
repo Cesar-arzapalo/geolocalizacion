5440ab487ba34e4a907e56a5e646395b400b0fe1
Aptir <cesar.arzapalo@unmsm.edu.pe>
2022-01-18 18:14:55 -0500

    aniadiendo correcicon de ruteo

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname+'/dist/Localizacion'));
+app.use(express.static(__dirname+'/dist/geolocalizacion'));
 app.get('*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/Localizacion/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/geolocalizacion/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
